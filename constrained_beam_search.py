a65f7b0c2eb125e5050def43203e50913adc6a8f
Anton Abilov <anton.abilov@gmail.com>
2022-05-01 01:46:53 -0400

    Added FactualLogitsProcessor to constrained beam search (POC)

--- constrained_beam_search.py
+++ constrained_beam_search.py
@@ -2,6 +2,10 @@ from typing import List
 from transformers import (
     AutoModelForSeq2SeqLM,
     AutoTokenizer,
+    Constraint,
+    LogitsProcessor,
+    LogitsProcessorList,
+    PhrasalConstraint,
     Seq2SeqTrainingArguments,
     Seq2SeqTrainer,
     DataCollatorForSeq2Seq,
@@ -10,10 +14,60 @@ from datasets import load_dataset
 from preprocess.tokenize_utils import tokenize_with_cache
 import argparse
 import torch
+from collections import defaultdict
+
+class FactualLogitsProcessor(LogitsProcessor):
+    r"""
+    [`FactualLogitsProcessor`] enforcing dynamic constraints on logits
+    Args:
+        min_length (`int`):
+            The minimum length below which the score of `eos_token_id` is set to `-float("Inf")`.
+        eos_token_id (`int`):
+            The id of the *end-of-sequence* token.
+    """
+
+    def __init__(self, tokenizer, num_beams, banned_token_ids=set()):
+        self.tokenizer = tokenizer
+        self.banned_token_ids = banned_token_ids
+        self.unfactual_tokens_by_seq_idx = defaultdict(lambda: [])
+        self.num_beams = num_beams
+
+    def is_factual_token(
+        self,
+        sequence,  # sequence generated so far
+        token_id,  # next token to be generated
+        token_probability,  # probability of token to be generated
+    ):
+        if token_id in self.banned_token_ids:
+            return False
+        return True
+
+    def __call__(
+        self, input_ids: torch.LongTensor, scores: torch.FloatTensor
+    ) -> torch.FloatTensor:
+        # for every beam (partially generated sentence)
+        for beam_idx, (beam_input_ids, beam_scores) in enumerate(
+            zip(input_ids, scores)
+        ):
+            # get the last token of this beam
+            # last_word = self.tokenizer.decode(beam_input_ids[-1])
+            top_k = beam_scores.topk(k=5)
+            for prob, idx in zip(top_k[0], top_k[1]):
+                if not self.is_factual_token(
+                    beam_input_ids[-1], idx.item(), prob.item()
+                ):
+                    scores[beam_idx, idx] = -float("inf")
+                    self.unfactual_tokens_by_seq_idx[beam_idx % self.num_beams].append((
+                        len(beam_input_ids),
+                        idx.item(),
+                        prob.item(),
+                    ))
+                    
+        return scores
 
 
 def entropy(p_dist: torch.Tensor) -> float:
-    """"
+    """ "
     Calculates Shannon entropy for a probability distribution
 
     Args:
@@ -24,13 +78,10 @@ def entropy(p_dist: torch.Tensor) -> float:
     """
     # add epsilon because log(0) = nan
     p_dist = p_dist.view(-1) + 1e-12
-    return - torch.mul(
-        p_dist,
-        p_dist.log()
-    ).sum(0).item()
+    return -torch.mul(p_dist, p_dist.log()).sum(0).item()
 
 
-def generate_summaries_with_beam_tree(
+def generate_summaries_with_constraints(
     model: AutoModelForSeq2SeqLM,
     tokenizer: AutoTokenizer,
     docs_to_summarize: List[str],
@@ -45,14 +96,19 @@ def generate_summaries_with_beam_tree(
         padding=True,
     )
     input_token_ids = inputs.input_ids.to(device)
-
+    factuality_enforcer = FactualLogitsProcessor(tokenizer, num_beams, banned_token_ids={
+        5295,  # ' Wales'
+        9652,  # ' Edinburgh'
+    })
     model_output = model.generate(
         input_token_ids,
         num_beams=num_beams,
-        # max_length=0,
         early_stopping=True,
         return_dict_in_generate=True,
         output_scores=True,
+        # remove_invalid_values=True,
+        logits_processor=LogitsProcessorList([factuality_enforcer])
+        # max_length=0,
     )
 
     generated_summaries = [
@@ -63,46 +119,50 @@ def generate_summaries_with_beam_tree(
     ]
 
     # reshape model_output scores to (n_seqs x seq len x n_beams x vocab)
-    model_beam_scores = torch.stack(model_output.scores).reshape(
-        len(model_output.scores),
-        len(generated_summaries),
-        num_beams,
-        -1
-    ).permute(1, 0, 2, 3)
+    model_beam_scores = (
+        torch.stack(model_output.scores)
+        .reshape(len(model_output.scores), len(generated_summaries), num_beams, -1)
+        .permute(1, 0, 2, 3)
+    )
     # Collect Beam Search Metadata
     beams_metadata = []
-    for seq_idx in range(model_output.sequences.shape[0]):
-        top_beam_indices = [x.item() for x in model_output.beam_indices[seq_idx]]
-        seq_beams = {
-            "beams": [list() for _ in range(num_beams)],
-            "selected_beam_indices": top_beam_indices
-        }
-        beams_metadata.append(seq_beams)
+    if model_output.beam_indices is not None:
+        for seq_idx in range(model_output.sequences.shape[0]):
+            print(model_output.beam_indices)
+            top_beam_indices = [x.item() for x in model_output.beam_indices[seq_idx]]
+            seq_beams = {
+                "beams": [list() for _ in range(num_beams)],
+                "selected_beam_indices": top_beam_indices,
+                "dropped_seqs": factuality_enforcer.unfactual_tokens_by_seq_idx[seq_idx]
+            }
+            beams_metadata.append(seq_beams)
 
-        for idx, output_token_id in enumerate(model_output.sequences[seq_idx][1:]):
-            # beam_idx = model_output.beam_indices[seq_idx][idx]
-            for beam_idx in range(num_beams):
-                beam_probs = torch.exp(model_beam_scores[seq_idx][idx][beam_idx])
-                beam_top_alternatives = []
-                top_probs = torch.topk(beam_probs, k=3)
-                for i, v in zip(top_probs.indices, top_probs.values):
-                    beam_top_alternatives.append(
+            for idx, output_token_id in enumerate(model_output.sequences[seq_idx][1:]):
+                # beam_idx = model_output.beam_indices[seq_idx][idx]
+                for beam_idx in range(num_beams):
+                    beam_probs = torch.exp(model_beam_scores[seq_idx][idx][beam_idx])
+                    beam_top_alternatives = []
+                    top_probs = torch.topk(beam_probs, k=num_beams)
+                    for i, v in zip(top_probs.indices, top_probs.values):
+                        beam_top_alternatives.append(
+                            {
+                                "token": tokenizer.decode(i),
+                                "token_id": i.item(),
+                                "probability": v.item(),
+                            }
+                        )
+                    # print("appending at", beam_idx)
+                    seq_beams["beams"][beam_idx].append(
                         {
-                            "token": tokenizer.decode(i),
-                            "token_id": i.item(),
-                            "probability": v.item(),
+                            "top_tokens": beam_top_alternatives,
+                            "entropy": entropy(beam_probs),
+                            # "token_id": output_token_id,
+                            # "token": tokenizer.decode(output_token_id),
+                            # "beam_token_prob": selected_beam_probs[output_token_id].item(),
+                            # "beam_idx": beam_idx.item(),
+                            # "token_in_input": output_token_id in input_set,
                         }
                     )
-                print("appending at", beam_idx)
-                seq_beams["beams"][beam_idx].append({
-                    "top_tokens": beam_top_alternatives,
-                    "entropy": entropy(beam_probs),
-                    # "token_id": output_token_id,
-                    # "token": tokenizer.decode(output_token_id),
-                    # "beam_token_prob": selected_beam_probs[output_token_id].item(),
-                    # "beam_idx": beam_idx.item(),
-                    # "token_in_input": output_token_id in input_set,
-                })
 
     return generated_summaries, beams_metadata
 
@@ -125,4 +185,8 @@ if __name__ == "__main__":
 
     xsum_test = load_dataset("xsum")["test"]
 
-    generate_summaries_with_beam_tree(model, tokenizer, xsum_test["document"][:2])
+    summaries, metadata = generate_summaries_with_constraints(
+        model, tokenizer, xsum_test["document"][2:4], num_beams=4
+    )
+
+    print(summaries)
